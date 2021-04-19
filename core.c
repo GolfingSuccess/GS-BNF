5858169da77f9622c8fa8e552b0f7868587293d0
GolfingSuccess <GolfingSuccess@users.noreply.github.com>
2021-04-19 12:38:20 +0300

    sanitization

--- core.c
+++ core.c
@@ -4,38 +4,17 @@
 
 static int is_letter(char x)
 {
-    return x == 'A' || x == 'B' || x == 'C' || x == 'D' ||
-           x == 'E' || x == 'F' || x == 'G' || x == 'H' ||
-           x == 'I' || x == 'J' || x == 'K' || x == 'L' ||
-           x == 'M' || x == 'N' || x == 'O' || x == 'P' ||
-           x == 'Q' || x == 'R' || x == 'S' || x == 'T' ||
-           x == 'U' || x == 'V' || x == 'W' || x == 'X' ||
-           x == 'Y' || x == 'Z' || x == 'a' || x == 'b' ||
-           x == 'c' || x == 'd' || x == 'e' || x == 'f' ||
-           x == 'g' || x == 'h' || x == 'i' || x == 'j' ||
-           x == 'k' || x == 'l' || x == 'm' || x == 'n' ||
-           x == 'o' || x == 'p' || x == 'q' || x == 'r' ||
-           x == 's' || x == 't' || x == 'u' || x == 'v' ||
-           x == 'w' || x == 'x' || x == 'y' || x == 'z';
+    return !!strchr("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", x);
 }
 
 static int is_digit(char x)
 {
-    return x == '0' || x == '1' || x == '2' || x == '3' ||
-           x == '4' || x == '5' || x == '6' || x == '7' ||
-           x == '8' || x == '9';
+    return !!strchr("0123456789", x);
 }
 
 static int is_symbol(char x)
 {
-    return x == '|' || x == ' ' || x == '!' || x == '#' ||
-           x == '$' || x == '%' || x == '&' || x == '(' ||
-           x == ')' || x == '*' || x == '+' || x == ',' ||
-           x == '-' || x == '.' || x == '/' || x == ':' ||
-           x == ';' || x == '>' || x == '=' || x == '<' ||
-           x == '?' || x == '@' || x == '[' || x == '\\' ||
-           x == ']' || x == '^' || x == '_' || x == '`' ||
-           x == '{' || x == '}' || x == '~';
+    return !!strchr("| !#$%&()*+,-./:;>=<?@[\\]^_`{}~", x);
 }
 
 static int is_character(char x)
