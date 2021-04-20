e027ec8d48eeaa56af5b512cedf3f3270a44d80e
GolfingSuccess <GolfingSuccess@users.noreply.github.com>
2021-04-20 15:51:11 +0300

    file output support

--- bnf-features.h
+++ bnf-features.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
 #include "bnf.h"
 
-void print_grammar_repr(bnf_grammar);
+void fprint_grammar_repr(bnf_grammar, FILE *);
+
+#define print_grammar_repr(gr) (fprint_grammar_repr((gr), stdout))
--- features.c
+++ features.c
@@ -2,26 +2,32 @@
 #include <string.h>
 #include "bnf.h"
 
-void print_grammar_repr(bnf_grammar gr)
+void fprint_grammar_repr(bnf_grammar gr, FILE *fp)
 {
     for (size_t rule = 0; rule < gr.rule_number; ++rule)
     {
-        printf("<%s> ::=", gr.rules[rule].name);
+        fprintf(fp, "<%s> ::=", gr.rules[rule].name);
         for (size_t list = 0; list < gr.rules[rule].expr.list_number; ++list)
         {
             if (list)
-                fputs(" |", stdout);
+                fputs(" |", fp);
             for (size_t term = 0; term < gr.rules[rule].expr.lists[list].term_number; ++term)
             {
-                putchar(' ');
-                printf(gr.rules[rule].expr.lists[list].terms[term].type == TRM_LIT
-                           ? strchr(gr.rules[rule].expr.lists[list].terms[term].value.literal, '"') ? "'%s'" : "\"%s\""
-                           : "<%s>",
-                       gr.rules[rule].expr.lists[list].terms[term].type == TRM_RULE
-                           ? gr.rules[rule].expr.lists[list].terms[term].value.rule->name
-                           : gr.rules[rule].expr.lists[list].terms[term].value.literal);
+                fputc(' ', fp);
+                fprintf(fp,
+                        gr.rules[rule].expr.lists[list].terms[term].type == TRM_LIT
+                            ? strchr(gr.rules[rule].expr.lists[list].terms[term].value.literal, '"') ? "'%s'" : "\"%s\""
+                            : "<%s>",
+                        gr.rules[rule].expr.lists[list].terms[term].type == TRM_RULE
+                            ? gr.rules[rule].expr.lists[list].terms[term].value.rule->name
+                            : gr.rules[rule].expr.lists[list].terms[term].value.literal);
             }
         }
-        putchar('\n');
+        fputc('\n', fp);
     }
+}
+
+int rule_match_test(bnf_rule rule, char *s, size_t size)
+{
+    // unimplemented
 }
